10da3c983901039d66f62e3ef2a64735c5a95c24
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-07-17 12:51:54 +0530

    make it better1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import app from './app.js'
 import dotenv from 'dotenv'
 import connectDB from "./config/database.js";
+import path from "path";
+import express from "express";
 
 dotenv.config({path: "backend/config/config.env"});
 
@@ -12,3 +14,21 @@ connectDB()
 app.listen(process.env.PORT,() =>{
   console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
+
+// --------------------------deployment------------------------------
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
