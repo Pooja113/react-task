408d529e9b237a8a3f89728bf648d2760e2dc052
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-07-18 22:04:33 +0530

    final

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-//const url = 'https://mern-task-test.herokuapp.com/joblist'
-const url = 'http://localhost:5000/joblist'
+const url = 'https://mern-task-test.herokuapp.com/joblist'
+//const url = 'http://localhost:5000/joblist'
 
 
 
